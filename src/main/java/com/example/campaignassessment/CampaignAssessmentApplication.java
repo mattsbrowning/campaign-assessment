f3873d61ece37b4c3b325eb048e56af8c4718032
Matthew Browning <mb@matthewb.org>
2026-03-12 21:01:12 +0000

    Initial scaffolding: Spring Boot 3.5, HATEOAS, validation, Bucket4j

---
+++ src/main/java/com/example/campaignassessment/CampaignAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.campaignassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignAssessmentApplication.class, args);
+	}
+
+}
